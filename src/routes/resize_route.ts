ff9f94ad0984390d9bc121153abd841d190cc3c4
umarsabra <omaarsaabra@outlook.com>
2022-06-22 23:26:29 +0200

    Added comments => resize_route

--- src/routes/resize_route.ts
+++ src/routes/resize_route.ts
@@ -6,30 +6,32 @@ import { resize } from "../utils";
 const router = express.Router();
 
 //Raw image array  assets/raw
-
 const images: string[] = ["beach", "cat", "city", "globe", "laptop", "phone"];
 
+//Middelware for valditating user input
 router.use((req, res, next) => {
-  //Validating the user required input
+  //Validating the user required input (name, width, height)
   if (!req.query.name || !req.query.width || !req.query.height) {
-    res.status(400);
+    res.status(400); //Bad Request
     res.json({
       error: "name, width and height are required!",
     });
 
     //Checking the data type
   } else if (!Number(req.query.width) || !Number(req.query.height)) {
-    res.status(400);
+    res.status(400); //Bad Request
     res.json({
       error: "width and height should be of type number!",
     });
 
     //Checking if the image exists
   } else if (!images.includes(req.query.name as string)) {
-    res.status(404);
+    res.status(404); //Not Found
     res.json({
       error: `there is no image with name: ${req.query.name}`,
     });
+
+    //Validation Passed
   } else {
     next();
   }
@@ -41,20 +43,24 @@ router.get("/", async (req, res) => {
   const height = Number(req.query.height);
   const target = String(req.query.name);
 
-  //Check cache for desired image size
-  /**
-   * Processed image have in it's name the width and height for refering to it in the future if the image has been processed before with the same dimensions
+  /*
+    Processed image have in it's name the width and height for refering to it in the future if the image has been processed before with the same dimensions
    */
-  const resized_image_name = `${target}_${width}_${height}.jpg`;
+
+  //Check cache for desired image size
+  const resized_image_name = `${target}_${width}_${height}.jpg`; //Expected image name
   const resized_image_path = path.resolve(
     "assets/processed",
     resized_image_name
   );
 
-  const isImageExist = fs.existsSync(resized_image_path);
+  const isImageExist = fs.existsSync(resized_image_path); //Returns true if path exist and false otherwise
 
+  //If image was found in the processed folder returns it directly
   if (isImageExist) {
     res.sendFile(resized_image_path);
+
+    //If image was not found
   } else {
     //Awaiting for the resizing process
     const output = await resize(width, height, target);
