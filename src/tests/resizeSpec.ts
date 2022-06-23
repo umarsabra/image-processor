91b2fb92686e9425c05d903c6de7b71c8a697630
umarsabra <omaarsaabra@outlook.com>
2022-06-23 11:29:41 +0200

    Prettier and ESLint Modified 1- Edited .eslint config file 2- Added .eslintignore file 3- Added both prettier and eslint scripts to package.json 4- Added format script to run eslint and prettier at the same time

--- src/tests/resizeSpec.ts
+++ src/tests/resizeSpec.ts
@@ -1,11 +1,11 @@
-import { resize } from '../utils';
+import { resize } from "../utils";
 
-describe('Testing the resize functionality', () => {
-  it('Check the output of the resized image ', async () => {
-    const output = (await resize(300, 300, 'cat')) as string;
+describe("Testing the resize functionality", () => {
+  it("Check the output of the resized image ", async () => {
+    const output = (await resize(300, 300, "cat")) as string;
     /*
         if the resize function returned an output of cat_300_300.jpg that means the function has successfully resized the image cat.jpg from the raw folder
      */
-    expect(output).toBe('cat_300_300.jpg');
+    expect(output).toBe("cat_300_300.jpg");
   });
 });
